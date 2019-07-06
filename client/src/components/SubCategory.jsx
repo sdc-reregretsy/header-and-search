770a50d59b1196850d90d53440d640b4fc88a76a
Brendan <bpdavitt@gmail.com>
2019-07-06 17:28:21 -0500

    Initial work refactoring to use react-bootstrap Navbar.

--- client/src/components/SubCategory.jsx
+++ client/src/components/SubCategory.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SubCategory = (props) => {
   const {subCategory} = props;
-  console.log(subCategory)
+  // console.log(subCategory)
   return (
     <div>{subCategory.name}</div>
   )
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,23 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import navCats from '../../navCats.json'
 import Departments from './components/Departments.jsx'
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
-class Navbar extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       searchTerm: ''
     }
   }
-  
+
   render() {
     console.log(navCats)
+    console.log(Navbar)
     return (
-      <div>
-        <Departments departments={navCats.departments} />
-      </div>
+      // <div>
+      //   <Departments departments={navCats.departments} />
+      // </div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Regretsy</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar>
     )
   }
 }
 
-ReactDOM.render(<Navbar />, document.getElementById('header'));
+ReactDOM.render(<App />, document.getElementById('header'));
