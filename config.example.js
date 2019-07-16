19ec3dbcccab784e73e494e6f32d20ba6a72f38c
Brendan <bpdavitt@gmail.com>
2019-07-15 20:06:05 -0500

    Add example config

---
+++ config.example.js
@@ -0,0 +1,17 @@
+module.exports = {
+  PORT: process.env.PORT,
+  HOST: process.env.HOST,
+  DBCONFIG: {
+    host: 'string',
+    user: 'string',
+    password: 'string',
+    database: 'string'
+  }
+};
+
+
+
+// host: 'localhost',
+  // user: 'student',
+  // password: 'student',
+// database: 'search'
