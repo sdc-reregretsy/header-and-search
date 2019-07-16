776219710ac12cce303f1476d8f40cf073aff033
Brendan <bpdavitt@gmail.com>
2019-07-16 17:59:51 -0500

    Deploy using EB and AWS RDS mysql db

--- database/index.js
+++ database/index.js
@@ -1,7 +1,14 @@
 const mysql = require('mysql');
 const mysqlConfig = require('../config.js');
 
-var connection = mysql.createConnection(mysqlConfig.DBCONFIG);
+// var connection = mysql.createConnection(mysqlConfig.DBCONFIG);
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT,
+  database:  process.env.RDS_DB_NAME
+})
 
 // connection.query(`CREATE TABLE items (listing_id int NOT NULL, title varchar(255) NOT NULL, PRIMARY KEY (listing_id));`);
 
