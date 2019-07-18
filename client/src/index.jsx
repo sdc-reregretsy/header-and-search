e8220ee87a34a383973eedf431e1745be0ec355a
Brendan <bpdavitt@gmail.com>
2019-07-18 11:14:33 -0500

    Make 2nd line of navbar responsive to resizing. Search bar is now cleared upon successful search

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -15,40 +15,8 @@ class App extends React.Component {
       allTitles: [],
       idDict: {}
     }
-    this.handleSearchChange = this.handleSearchChange.bind(this)    
+    this.handleSearchChange = this.handleSearchChange.bind(this)
     this.bc = new BroadcastChannel('regretfully');
-  }
-
-
-  // NOTE: Typeahead change handler works differently than a normal input field
-  handleSearchChange(selected) {
-    console.log(selected[0]);
-    console.log('Change Handler entered')
-    this.setState({searchTerm: selected[0]})
-  }
-
-  //On submit need to lookup the listing_id of the selected term and pass it to other microservices
-  handleSearchSubmit() {
-    let listing_id = this.state.idDict[this.state.searchTerm]
-    if(listing_id === undefined) {
-      return alert('Please pick an option from the search bar')
-    }
-    console.log('You pressed the button but nothing happened')
-    console.log(`Search Term: ${this.state.searchTerm} \nReferences id: ${listing_id}`)
-    this.bc.postMessage(listing_id);
-
-  }
-
-  handleItems(items) {
-    const titleArr = [];
-    const titleDict = {};
-    for (let element of items) {
-      titleDict[element.title] = element.listing_id;
-      titleArr.push(element.title);
-    }
-    // console.log(titleArr);
-    // console.log(titleDict);
-    this.setState({ allTitles: titleArr, idDict: titleDict });
   }
 
   componentDidMount() {
@@ -63,14 +31,46 @@ class App extends React.Component {
       })
   }
 
+  // NOTE: Typeahead change handler works differently than a normal input field
+  handleSearchChange(selected) {
+    console.log(selected[0]);
+    console.log('Change Handler entered')
+    this.setState({ searchTerm: selected[0] })
+  }
+
+  //On submit need to lookup the listing_id of the selected term and pass it to other microservices
+  handleSearchSubmit() {
+    let listing_id = this.state.idDict[this.state.searchTerm]
+    if (listing_id === undefined) {
+      return alert('Please pick an option from the search bar')
+    }
+    console.log(`Search Term: ${this.state.searchTerm} \nReferences id: ${listing_id}`)
+    this.bc.postMessage(listing_id);
+    this.typeahead.getInstance().clear()
+    this.setState({ searchTerm: '' });
+  }
+
+  handleItems(items) {
+    const titleArr = [];
+    const titleDict = {};
+    for (let element of items) {
+      titleDict[element.title] = element.listing_id;
+      titleArr.push(element.title);
+    }
+    // console.log(titleArr);
+    // console.log(titleDict);
+    this.setState({ allTitles: titleArr, idDict: titleDict });
+  }
+
+
   render() {
     // console.log(navCats)
     // console.log(this.bc)
 
-    this.bc.onmessage = function(ev) {
+    this.bc.onmessage = function (ev) {
       console.log(ev.data)
     }
-    
+
     return (
       <>
         <Navbar bg="light" expand="sm" className="align-items-center">
@@ -85,13 +85,14 @@ class App extends React.Component {
                 options={this.state.allTitles}
                 onChange={(selected) => { this.handleSearchChange(selected) }}
                 id="typeahead-search"
+                ref={(typeahead) => this.typeahead = typeahead}
               />
               <InputGroup.Append>
                 {/* &#x1F50E; is another magnifying glass option*/}
                 <Button variant="outline-secondary"
                   className="searchBtn"
-                  onClick={()=>{this.handleSearchSubmit()}}
-                  >
+                  onClick={() => { this.handleSearchSubmit() }}
+                >
                   &#8981;
                   </Button>
 
@@ -99,12 +100,12 @@ class App extends React.Component {
             </InputGroup>
           </Form>
           <Nav className="justify-content-end" style={{ width: "50%" }}>
-            <Nav.Link className="navLink" onClick={()=>{alert('Under Construction: Come Back Later')}}>Sell on Etsy</Nav.Link>
-            <Nav.Link className="navLink" onClick={()=>{alert('Sorry, Registration is Down')}}>Register</Nav.Link>
-            <Button variant="outline-success" onClick={()=>{alert('Sorry, Sign-In is down')}}>Sign In</Button>
+            <Nav.Link className="navLink" onClick={() => { alert('Under Construction: Come Back Later') }}>Sell on Etsy</Nav.Link>
+            <Nav.Link className="navLink" onClick={() => { alert('Sorry, Registration is Down') }}>Register</Nav.Link>
+            <Button variant="outline-success" onClick={() => { alert('Sorry, Sign-In is down') }}>Sign In</Button>
           </Nav>
         </Navbar>
-        <Navbar bg="light">
+        <Navbar bg="light" expand="lg">
           <Departments departments={navCats.departments} />
         </Navbar>
       </>
